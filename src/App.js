9ac95f762faec02057ead57155b0526e0ea4e599
Ash <ashley.bergsma42@gmail.com>
2021-03-04 12:26:46 -0700

    Adds PanelLeft component
    While building the Dashboard there was a considerable amount of code going into the component itself.
    
    I made the choice to modularize the elements of the utility panel at the left--hand side of the user dashboard. That way, if I want this utility panel accessible on other display, I can reuse it.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css'
 
-import Dashboard from './components/Dashboard'; 
+import Dashboard from './components/dashboard/Dashboard'; 
 import Hero from './components/Hero'; 
 import Nav from './components/Nav'; 
 import Login from './components/Login'; 
