1df7595d3b7503fa11b260055385ab04b438389b
Ash <ashley.bergsma42@gmail.com>
2021-03-19 17:01:00 -0700

    Adds to HeroDetails component
    More landing page details and graphics have been added. Trying to spice it up!

--- src/components/LandingPage/HeroDetails.js
+++ src/components/LandingPage/HeroDetails.js
@@ -4,12 +4,13 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from '../../styles/HeroDetailsStyles';
 
 import curveGraphic from '../../resources/images/forgetting-curve-graphic.jpg';
+import spacedRepGraphic from '../../resources/images/spaced-rep-graphic.jpg'
 
 function HeroDetails({ classes }) {
 
     return (
         <React.Fragment>
-            <Paper className={classes.root}>
+            <Paper className={classes.sectionOne}>
                 <Box className={classes.textBox}>
                     <Typography className={classes.subtitle}>Stop cramming.</Typography>
                     <Typography className={classes.bodyText}>
@@ -21,6 +22,20 @@ function HeroDetails({ classes }) {
                 <Box className={classes.imgBox}>
                     <img src={curveGraphic} alt="Forgetting Curve Graph" className={classes.image} /> 
                 </Box>
+            </Paper>
+            <Paper className={classes.sectionTwo}>
+                <Box className={classes.imgBox}>
+                    <img src={spacedRepGraphic} alt="Spaced repitition visual" className={classes.image} /> 
+                </Box>
+                <Box className={classes.textBox}>
+                    <Typography className={classes.subtitle}>Start Learning.</Typography>
+                    <Typography className={classes.bodyText}>
+                        
+                        Using spaced repitition, studied and written about by Sebastien Leitner in the 1970's, we can flatten the Forgetting Curve, truly learning the material at hand.
+                    
+                    </Typography>
+                </Box>
+                
             </Paper>
         </React.Fragment>
     )
