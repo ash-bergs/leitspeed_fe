51549a138b84c853d4b5c55d1c7d3e6b341deb0c
Ash <ashley.bergsma42@gmail.com>
2021-06-12 02:45:05 -0700

    Returns to the project
    There's a lot to do, stuff to change, things I've learned from reading a 400 page book about React and taking another course.

--- src/components/Cards/subComponents/Card.js
+++ src/components/Cards/subComponents/Card.js
@@ -1,68 +1,79 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 //*material UI hook wraps component with styles (CardStyles.js)
-import { withStyles } from '@material-ui/core/styles'; 
-import styles from './CardStyles'; 
+import { withStyles } from '@material-ui/core/styles';
+import styles from './CardStyles';
 
-import ReactCardFlip from 'react-card-flip'; 
-//* material UI components 
-import { 
-    Avatar, 
-    Button, 
-    Card, 
-    CardActions, 
-    CardContent, 
-    CardHeader, 
-    Typography 
-} from '@material-ui/core'; 
-
+import ReactCardFlip from 'react-card-flip';
+//* material UI components
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Typography,
+} from '@material-ui/core';
 
 const FlashCard = ({ card, classes }) => {
-    // isFlipped state controls the react-card-flip component, false on start 
-    const [isFlipped, setIsFlipped] = useState(false); 
+  // isFlipped state controls the react-card-flip component, false on start
+  //* isFlipped is connected to the ReactCardFlip component - it handles the transition between the front and back of a card
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    // click handler - toggles the isFlipped state, flipping the card to reveal the text on the back
-    const flipCard = (event) => {
-        event.preventDefault(); 
-        setIsFlipped(!isFlipped); 
-    }
-    //* isFlipped is connected to the ReactCardFlip component - the parents of two separate containing divs - the front and the back.
-    //* front and back of each each is its own Card component - the sides have slightly different functionality
-    //* each "face" of the card is connected to the flipCard handler fn
-    //! It may be best to add a "flip" button, rather than making the whole card clickable? Otherwise features like definitions can't really be used (without flipping the card)
-    return (
-        <React.Fragment>
-        
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                <Card onClick={flipCard} className={classes.root} variant="outlined">
-                    <CardHeader
-                        avatar={<Avatar className={classes.small} aria-label="subject">CSS</Avatar>}
-                        className={classes.header}
-                        title="Deck Subject Here"
-                    />
-                    <CardContent className={classes.content}>
-                        <Typography className={classes.text} variant="h4">{card.card_front}</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" variant="outlined" color="secondary">Delete</Button>
-                        <Button size="small" variant="outlined">Edit</Button>
-                    </CardActions>
-                </Card>
+  // click handler - toggles the isFlipped state, flipping the card to reveal the text on the back
+  const flipCard = (event) => {
+    event.preventDefault();
+    setIsFlipped(!isFlipped);
+  };
 
-                <Card onClick={flipCard} className={classes.root}>
-                    <CardContent className={classes.content}>
-                        <Typography className={classes.text} variant="h4">{card.card_back}</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" variant="outlined">Edit</Button>
-                    </CardActions>
-                </Card>
-            </ReactCardFlip>
+  //* each "face" of the card is connected to the flipCard handler fn
+  //! It may be best to add a "flip" button, rather than making the whole card clickable? Otherwise features like definitions can't really be used (without flipping the card)
+  return (
+    <React.Fragment>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <Card onClick={flipCard} className={classes.root} variant="outlined">
+          <CardHeader
+            avatar={
+              <Avatar className={classes.small} aria-label="subject">
+                CSS
+              </Avatar>
+            }
+            className={classes.header}
+            title="Deck Subject Here"
+          />
+          <CardContent className={classes.content}>
+            <Typography className={classes.text} variant="h4">
+              {card.card_front}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined" color="secondary">
+              Delete
+            </Button>
+            <Button size="small" variant="outlined">
+              Edit
+            </Button>
+          </CardActions>
+        </Card>
 
-        </React.Fragment>
-    ); 
-}
+        <Card onClick={flipCard} className={classes.root}>
+          <CardContent className={classes.content}>
+            <Typography className={classes.text} variant="body1">
+              {card.card_back}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined">
+              Edit
+            </Button>
+          </CardActions>
+        </Card>
+      </ReactCardFlip>
+    </React.Fragment>
+  );
+};
 
-export default withStyles(styles)(FlashCard); 
+export default withStyles(styles)(FlashCard);
 
 /* -------------------------------------------------------------------------- */
 /*                             proposed structure                             */
@@ -90,14 +101,14 @@ export default withStyles(styles)(FlashCard);
 /* -------------------------------------------------------------------------- */
 
 /* --------------------------- Card Design Update --------------------------- */
-//* Now that the build has seen some progress, the shape of how things will fit together is starting to emerge. 
+//* Now that the build has seen some progress, the shape of how things will fit together is starting to emerge.
 //* 3/6/21 - Updating the cards to appear more like a flashcard - adding edit and delete button [for now these are non-functional]
 //TODO - add a Collapse MUI element to the back of the card - this will render "notes" from the card (if it has any)
-//TODO - media sizing - when on mobile the variant of the Typography component should be smaller. h4 is blocky and ugly on small screens. 
+//TODO - media sizing - when on mobile the variant of the Typography component should be smaller. h4 is blocky and ugly on small screens.
 
 //TODO - Answer the following...
 //? How to use pagination to show one card at a time
-//? How can a user flag a card they want to study more, or move one they no longer want to study? 
-//? How can I track the time a user spends studying a deck, and studying overall? Start by solving for tracking time overall. 
+//? How can a user flag a card they want to study more, or move one they no longer want to study?
+//? How can I track the time a user spends studying a deck, and studying overall? Start by solving for tracking time overall.
 
 //! We have a problem here - clicking the buttons causes the card to flip - we need a way to separate the functionality - edit and delete must not cause the react-card-flip function to trigger
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,218 +1,24 @@
-import React, { useEffect, useState } from 'react'; 
-import { createServer } from 'miragejs'; 
-import axios from 'axios'; 
+import React from 'react';
+//* material UI components
+import { Container, Box } from '@material-ui/core';
 
-//* material UI components 
-import { Container, Box} from '@material-ui/core';
-//* material UI hooks
-import { withStyles } from '@material-ui/core/styles'; 
+//* material UI hook
+import { withStyles } from '@material-ui/core/styles';
 
-//* dev built components 
-import Cards from '../Cards'; 
-import UserPanel from '../UserPanel'; 
-import styles from './DashboardStyles'; 
+//* dev built components
+import Cards from '../Cards';
+//import UserPanel from '../UserPanel';
+import styles from './DashboardStyles';
 
-/* -------------------------------------------------------------------------- */
-/*                             About the Dashboard                            */
-// * Dashboard is a top-level component
-// It's concerned with state, like cards, and actions on cards, decks, etc.
-
-// From Material UI: Container, Box 
-/* -------------------------------------------------------------------------- */
-
-//* ---------------------------- 📝 About MirageJS --------------------------- */
-// MirageJS is a library to mock an API in a front end build. I'm using it here so I can start putting things together in this app while I build the API. And because I think it would be a good bit of knowledge to have in my back pocket. 
-// Installed as a DEVDEPENENCY 
-// The Mirage server is set up in this file, the Dashboard, because this will be the component making these calls anyway
-// I might abstract it to its own directly in the future. 
-
-//* Begin Mirage mock API 
-let server = createServer(); 
-//* GET route - /cards - all cards 
-server.get("/api/cards", { data: [
-    {
-        id: 1, 
-        user_id: 1, 
-        subject_id: 1, 
-        card_front: "What does enumerate do?", 
-        card_back: " Enumerate adds a counter to an iterable, and returns it in the form of an enumerate object. The object can be used directly in for loops, or converted into a list of tuples (using the list() method).", 
-        card_notes: ["Python method", "Programming"], 
-        active: true, 
-        public: true
-    }, 
-    {
-        id: 2, 
-        user_id: 1, 
-        subject_id: 1, 
-        card_front: "What are keyword arguments?", 
-        card_back: "Keyword arguments are named - meaning that their position does not matter but their name does", 
-        card_notes: ["Programming"], 
-        active: true, 
-        public: true
-    }, 
-    {
-        id: 3, 
-        user_id: 1, 
-        subject_id: 1, 
-        card_front: "What are positional arguments?", 
-        card_back: "Positional arguments can be named anything (banana words), but their position is important!", 
-        card_notes: ["Programming"], 
-        active: true, 
-        public: true
-    }, 
-    {
-        id: 4, 
-        user_id: 1, 
-        subject_id: 2, 
-        card_front: "Name a feature that makes objects such a widely used data structure", 
-        card_back: "Flexible storage. Intuitive storage (key-value pairs). Fast lookup time!", 
-        card_notes: ["Programming", "Think about Big O for this answer"], 
-        active: true, 
-        public: true
-    }, 
-    {
-        id: 5, 
-        user_id: 2, 
-        subject_id: 3, 
-        card_front: "What makes purple cauliflower purple?", 
-        card_back: "A surplus of anthocyanin! This antioxidant also gives red wine and grapes their coloring", 
-        card_notes: ["Antioxidants", "Purple foods!"], 
-        active: true, 
-        public: true
-    }
-]}); 
-//* GET route - /decks - all decks
-server.get("/api/decks", { data: [
-    {
-        id: 1,
-        name: "Computer Science", 
-        public: false
-    },
-    {
-        id: 2,
-        name: "American History", 
-        public: false
-    },
-    {
-        id: 3,
-        name: "Food Science", 
-        public: true
-    },
-    {
-        id: 4,
-        name: "CSS", 
-        public: true
-    },
-    {
-        id: 5,
-        name: "JavaScript", 
-        public: false
-    },
-]}); 
-
-//* Dummy data (leitDecks)
-const leitDecks = [
-    {
-        id: 1, 
-        name: "Phase 1"
-    },
-    {
-        id: 2, 
-        name: "Phase 2"
-    },
-    {
-        id: 3, 
-        name: "Phase 3"
-    },
-    {
-        id: 4, 
-        name: "Phase 4"
-    },
-    {
-        id: 5, 
-        name: "Phase 5"
-    }, 
-]; 
-
-// * classes refers to MUI style overrides - style classes 
-function Dashboard({ classes }) {
-    // useState to create a slice of state for CARDS 
-    // useEffect will make a fetch call - the Mirage server will intercept the call, and return the cards object defined in the route handler
-    // as normal, cards is init as an empty array 
-    const [cards, setCards] = useState([]); 
-    const [userData, setUserData] = useState([]); 
-
-    
-    //TODO Implement one state update, to avoid out-of-sync behavior 
-    // Using some logic I found on stackOverflow, I'm going to try using a self-invoking function to set the initial state 
-    // the data needed on render is all related to one thing - the user. 
-    // we need CARDS and DECKS right now - we'll worry about LeitDecks later 
-    useEffect(() => {
-        (async () => {
-            const data1 = await axios.get("/api/cards");
-            const data2 = await axios.get("/api/decks");
-            setUserData({ cards: data1.data, decks: data2.data }); 
-            // ? now I have the two chunks of data the page needs.. but how do I best deal it out and update it?
-        })()
-    }, []);
-
-    console.log("userData", userData); 
-    console.log("cards", cards)
-
-    // ! This is what I was doing originally and want to replace with one useEffect and state update. 
-    useEffect(() => {
-        fetch("/api/cards")
-            .then((res) => res.json())
-            .then((json) => {
-                setCards(json.data)
-            })
-    }, []); 
-    
-    return (
-        // Container centers content horizontally 
-        // bounded by the `maxWidth` property - large, medium, small. etc
-        <Container className={classes.root} fixed>
-            
-            {/* Pass DECKS to PanelLeft, and finally to the DecksView child component */}
-            {/* Pass LEIT-DECKS to PanelLeft, and finally to the DecksView child component */}
-            <UserPanel decks={leitDecks} leitDecks={leitDecks} />
-
-            {/* Paper component gives us something that feels like paper, flat, white, etc*/}
-            <Box className={classes.box}>
-            {/* Will there be a ternary here to update different deck selections? if selectedDeck is null, then render Cards, otherwise render that decks cards.... Cards will probably need to be refactored to render dynamic sets */}
-                <Cards cards={cards} /> 
-            </Box>
-
-        </Container>
-    )
+// * classes refers to MUI style overrides - style classes
+function Dashboard({ cards, classes }) {
+  return (
+    <Container className={classes.root} fixed>
+      <Box className={classes.box}>
+        <Cards cards={cards} />
+      </Box>
+    </Container>
+  );
 }
 
 export default withStyles(styles)(Dashboard);
-
-//! 📝 Note 
-// branch: testing/mirage-setup
-// I'm currently trying to implement the MirageJS Server to mock an API in the front end 
-// The motivation for this move is to continue forward momentum on building the UI while the API is being finished. 
-// Currently, the architecture of the database is unclear, and being able to play with solution via Mirage will help me get a better idea of how to structure the API 
-// The API is also currently non-functional //TODO - get some help on this, perhaps ask Harrison to take a look 
-
-// ? Problem One: Making multiple fetch calls, and updating multiple slices of state simultaneously. 
-// I connected the CARDS state to the Mirage server, useEffect is currently updating the cards state on component mount 
-// But I also need to make a call for DECKS and LEITDECKS at the same time, ideally updating them together. 
-
-/*
-First I explored using PROMISE.ALL in the useEffect.
-I thought I could define an array of urls, the endpoints. Something like: 
-
-useEffect(() => {
-    const urls = ["/api/cards", "/api/decks", "/api/leitDecks"]; 
-    Promise.all(urls.map(url => fetch(url)))
-        .then(responses => Promise.all(responses.map(res => res.data))
-            .then(data => {
-                ! but here I lost the trail I was on... and I'm fairly certain I'm thinking of this chain of Promise objects incorrectly. 
-                ! I would need to loop through all the setter functions, and make sure they get the RIGHT data object....
-                ? this seems like a very verbose way of doing something that could be more easily accomplished by a React hook - maybe the useReducer hook?
-            }))
-        })
-    }, []); 
-*/
